7cd72f30c86f81437a53a9e093b71e2876b5bbec
psterner <psterner@hotmail.com>
2023-08-09 13:41:27 +0200

    Add Multisite Setup

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,6 +6,20 @@
 use Roots\WPConfig\Config;
 use function Env\env;
 
+/**
+ * Multisite
+ */
+Config::define('WP_ALLOW_MULTISITE', true);
+
+
+Config::define( 'MULTISITE', true );
+Config::define( 'SUBDOMAIN_INSTALL', true );
+Config::define( 'DOMAIN_CURRENT_SITE', 'multisite.local' );
+Config::define( 'PATH_CURRENT_SITE', '/' );
+Config::define( 'SITE_ID_CURRENT_SITE', 1 );
+Config::define( 'BLOG_ID_CURRENT_SITE', 1 );
+
+
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
@@ -13,6 +27,7 @@ Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
+
 
 ini_set('display_errors', '1');
 
